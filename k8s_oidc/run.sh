eadab6541acd4b3414dc3aa17c2cf5beb6553abf
Sandip Gahlot <sgahlot@redhat.com>
2023-04-11 20:14:20 -0400

    added option to provide vars using file instead of prompt

--- k8s_oidc/run.sh
+++ k8s_oidc/run.sh
@@ -59,23 +59,6 @@ function display_menu_and_get_choice() {
 }
 
 
-#export subscription_id=<<subscription_id>>
-#export name_prefix=<<name_prefix>>
-#export resource_group_name=<<resource_group_name>>
-#export aro_cluster_name=<<aro_cluster_name>>
-#terraform import \
-#     -var "name_prefix=$name_prefix" \
-#     -var "cluster_name=$aro_cluster_name" \
-#     -var "resource_group_name=$resource_group_name" \
-#     azapi_resource.aro_cluster \
-#     /subscriptions/$subscription_id/resourceGroups/$resource_group_name/providers/Microsoft.RedHatOpenShift/OpenShiftClusters/$aro_cluster_name
-#
-# terraform apply \
-#     -var "name_prefix=$name_prefix" \
-#     -var "cluster_name=$aro_cluster_name" \
-#     -var "resource_group_name=$resource_group_name"
-
-
 function call_terraform_for_plan_or_apply() {
   local op="$1"
 
@@ -95,92 +78,101 @@ function call_terraform_for_plan_or_apply() {
 
 function run_terraform() {
   local op="$1"
+  local FILE_OPTION="file"
 
   local TMP_VAR=""
-  local resourcePrefix="test001"
-  read -p "Please enter resource prefix (default [$resourcePrefix]): " TMP_VAR
-  resourcePrefix="${TMP_VAR:-$resourcePrefix}"
+  local varFileOrPromptForVars=""
+  while [[ "$varFileOrPromptForVars" != "${FILE_OPTION}" && "$varFileOrPromptForVars" != "prompt" ]]
+  do
+    read -p "For most of the variables, use file or prompt (file/prompt): " TMP_VAR
+    varFileOrPromptForVars="${TMP_VAR:-$varFileOrPromptForVars}"
+  done
 
-  local TMP_VAR=""
-  local aroResourceGroup="${resourcePrefix}RG"
-  read -p "Please enter ARO resource group (default [$aroResourceGroup]): " TMP_VAR
-  aroResourceGroup="${TMP_VAR:-$aroResourceGroup}"
+#  if [ "$varFileOrPromptForVars" != "${FILE_OPTION}" -o "$op" == "$IMPORT" ]; then
+    local resourcePrefix="test001"
+    read -p "Please enter resource prefix (default [$resourcePrefix]): " TMP_VAR
+    resourcePrefix="${TMP_VAR:-$resourcePrefix}"
 
-  TMP_VAR=""
-  local aroClusterName="${resourcePrefix}Aro"
-  read -p "Please enter ARO cluster name (default [$aroClusterName]): " TMP_VAR
-  aroClusterName="${TMP_VAR:-$aroClusterName}"
+    local TMP_VAR=""
+    local aroResourceGroup="${resourcePrefix}RG"
+    read -p "Please enter ARO resource group (default [$aroResourceGroup]): " TMP_VAR
+    aroResourceGroup="${TMP_VAR:-$aroResourceGroup}"
 
-  TMP_VAR=""
-  local location="canadacentral"
-  read -p "Please enter location (default [$location]): " TMP_VAR
-  location="${TMP_VAR:-$location}"
-
-  TMP_VAR=""
-  local tenantId="NOT_SET"
-  read -p "Please enter tenantId (default [$tenantId]): " TMP_VAR
-  tenantId="${TMP_VAR:-$tenantId}"
-
-  TMP_VAR=""
-  local clientId="NOT_SET"
-  read -p "Please enter clientId (default [$clientId]): " TMP_VAR
-  clientId="${TMP_VAR:-$clientId}"
-
-  TMP_VAR=""
-  local clientSecret="NOT_SET"
-  read -p "Please enter clientSecret (default [$clientSecret]): " TMP_VAR
-  clientSecret="${TMP_VAR:-$clientSecret}"
+    TMP_VAR=""
+    local aroClusterName="${resourcePrefix}Aro"
+    read -p "Please enter ARO cluster name (default [$aroClusterName]): " TMP_VAR
+    aroClusterName="${TMP_VAR:-$aroClusterName}"
+#  fi
 
   local extraOptions=""
 
-  if [ "$op" == "$PLAN" ]; then
-   extraOptions="-out ${MAIN_PLAN_FILE}"
-  elif [ "$op" == "$APPLY" -o "$op" == "$DESTROY"  ]; then
-   extraOptions="-auto-approve"
-  elif [ "$op" == "$IMPORT" ]; then
-     local subscriptionId=""
-     while [[ -z "$subscriptionId" ]]
-     do
-       read -p "Please enter subscriptionId: " subscriptionId
-     done
+  if [ "$varFileOrPromptForVars" == "file" ]; then
+    extraOptions="-var-file=custom.tfvars"
+  else
+    TMP_VAR=""
+    local location="canadacentral"
+    read -p "Please enter location (default [$location]): " TMP_VAR
+    location="${TMP_VAR:-$location}"
 
-     extraOptions="azapi_resource.aro_cluster /subscriptions/$subscriptionId/resourceGroups/$aroResourceGroup/providers/Microsoft.RedHatOpenShift/OpenShiftClusters/$aroClusterName"
+    TMP_VAR=""
+    local tenantId="NOT_SET"
+    read -p "Please enter tenantId (default [$tenantId]): " TMP_VAR
+    tenantId="${TMP_VAR:-$tenantId}"
+
+    TMP_VAR=""
+    local clientId="NOT_SET"
+    read -p "Please enter clientId (default [$clientId]): " TMP_VAR
+    clientId="${TMP_VAR:-$clientId}"
+
+    TMP_VAR=""
+    local clientSecret="NOT_SET"
+    read -p "Please enter clientSecret (default [$clientSecret]): " TMP_VAR
+    clientSecret="${TMP_VAR:-$clientSecret}"
+
+    printf "\n   -> key ids/values used"
+    for i in op aroResourceGroup aroClusterName location
+    do
+      printf "\n     - $i=${!i}"
+    done
+
+    extraOptions="-var \"resource_group_name=$aroResourceGroup\""
+    extraOptions="${extraOptions} -var \"cluster_name=$aroClusterName\""
+    extraOptions="${extraOptions} -var \"region=$location\""
+    extraOptions="${extraOptions} -var \"tenant_id=\"$tenantId\"\""
+    extraOptions="${extraOptions} -var \"client_id=\"$clientId\"\""
+    extraOptions="${extraOptions} -var \"client_secret=\"$clientSecret\"\""
   fi
 
-  printf "\n   -> key ids/values used"
-  for i in op aroResourceGroup aroClusterName location extraOptions
-  do
-    printf "\n     - $i=${!i}"
-  done
+  if [ "$op" == "$PLAN" ]; then
+   extraOptions="-out ${MAIN_PLAN_FILE} ${extraOptions}"
+  elif [ "$op" == "$APPLY" -o "$op" == "$DESTROY"  ]; then
+   extraOptions="-auto-approve ${extraOptions}"
+  elif [ "$op" == "$IMPORT" ]; then
+    local subscriptionId=""
+    while [[ -z "$subscriptionId" ]]
+    do
+     read -p "Please enter subscriptionId: " subscriptionId
+    done
+
+    extraOptions="${extraOptions} azapi_resource.aro_cluster /subscriptions/$subscriptionId/resourceGroups/$aroResourceGroup/providers/Microsoft.RedHatOpenShift/OpenShiftClusters/$aroClusterName"
+  fi
+
 
   printf "\n\n -> Running terraform $op command...\n"
 
   set -x
   terraform $op \
     -compact-warnings \
-    -var "resource_group_name=$aroResourceGroup" \
-    -var "cluster_name=$aroClusterName" \
-    -var "region=$location" \
-    -var "tenant_id=\"$tenantId\"" \
-    -var "client_id=\"$clientId\"" \
-    -var "client_secret=\"$clientSecret\"" \
     $extraOptions
-
-#    -var "name_prefix=$resourcePrefix" \
-
-  #  terraform import \
-  #       -var "name_prefix=$name_prefix" \
-  #       -var "cluster_name=$aro_cluster_name" \
-  #       -var "resource_group_name=$resource_group_name" \
-  #       azapi_resource.aro_cluster \
-  #       /subscriptions/$subscription_id/resourceGroups/$resource_group_name/providers/Microsoft.RedHatOpenShift/OpenShiftClusters/$aro_cluster_name
-  #
-  # terraform apply \
-  #     -var "name_prefix=$name_prefix" \
-  #     -var "cluster_name=$aro_cluster_name" \
-  #     -var "resource_group_name=$resource_group_name"
-
   set +x
 }
+
+#    -var "resource_group_name=$aroResourceGroup" \
+#    -var "cluster_name=$aroClusterName" \
+#    -var "region=$location" \
+#    -var "tenant_id=\"$tenantId\"" \
+#    -var "client_id=\"$clientId\"" \
+#    -var "client_secret=\"$clientSecret\"" \
+
 
 display_menu_and_get_choice
